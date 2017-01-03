ef00da1361191b2e4a8e530fcf34d24b078adf60
Denis <denis.glod@mail.ru>
2017-01-03 17:29:06 +0300

    Added function showTable and delete class TableStatusBean

--- Training Management System/controllers/src/main/webapp/script/admin.js
+++ Training Management System/controllers/src/main/webapp/script/admin.js
@@ -1,4 +1,32 @@
 /**
  * @author denis
  */
+function showTable(key) {
+	switch (key) {
+	case 1:
+		document.getElementById("users-table").hidden = false;
+		document.getElementById("courses-table").hidden = true;
+		document.getElementById("groups-table").hidden = true;
+		document.getElementById("data-groups-table").hidden = true;
+		break;
+	case 2:
+		document.getElementById("users-table").hidden = true;
+		document.getElementById("courses-table").hidden = false;
+		document.getElementById("groups-table").hidden = true;
+		document.getElementById("data-groups-table").hidden = true;
+		break;
+	case 3:
+		document.getElementById("users-table").hidden = true;
+		document.getElementById("courses-table").hidden = true;
+		document.getElementById("groups-table").hidden = false;
+		document.getElementById("data-groups-table").hidden = true;
+		break;
+	case 4:
+		document.getElementById("users-table").hidden = true;
+		document.getElementById("courses-table").hidden = true;
+		document.getElementById("groups-table").hidden = true;
+		document.getElementById("data-groups-table").hidden = false;
+		break;
+	}
 
+}
